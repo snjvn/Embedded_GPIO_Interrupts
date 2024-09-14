36e1cd5e11864e3a3c2a9555c57eb887cf62be33
Pavini Prasad <paviniprasad@gmail.com>
2024-09-15 00:34:45 +0530

    SW2 - Level sensitive, SW1- Edge sensitive

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(void)
     GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
 
     NVIC_EN0_R = 0x40000000; // 30th bit controls PORTF
-    GPIO_PORTF_IS_R = 0x00; // interrupt sensitivity - edge
+    GPIO_PORTF_IS_R = 0x10; // sw2 level sensitive; sw1 edge sensitive
     GPIO_PORTF_IBE_R = 0x00; //Single edge
     GPIO_PORTF_IEV_R = 0x11; //Rising edge
     GPIO_PORTF_IM_R = 0x11; // unmasking both switches
@@ -33,7 +33,13 @@ int main(void)
 }
 
 void GPIOInterrupt(){
-    GPIO_PORTF_DATA_R = 0x08;
+    if(GPIO_PORTF_RIS_R & 0x10){
+        GPIO_PORTF_DATA_R = 0x08;
+    }
+    if(GPIO_PORTF_RIS_R & 0x01){
+        GPIO_PORTF_DATA_R ^= 0x04;
+        GPIO_PORTF_ICR_R = 0x01;
+    }
     NVIC_EN0_R = 0x00000000; // 30th bit controls PORTF
     GPIO_PORTF_IM_R = 0x00; // masking both switches
 
